6a1ec374b8422fc6ed715ea07c9d6d0f799a9c9b
EbenZhang <zhangyingneng@gmail.com>
2015-02-22 19:47:22 +0800

    MessageBoxHelper

---
+++ MessageBoxHelper.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+
+namespace WpfCommon
+{
+    public static class MessageBoxHelper
+    {
+        public static void ShowError(Window parent, string msg, string title="Error")
+        {
+            MessageBox.Show(parent, msg, title, MessageBoxButton.OK, MessageBoxImage.Error);
+        }
+        public static MessageBoxResult ShowConfirmation(Window parent, string msg, string title = "Confirm")
+        {
+            return MessageBox.Show(parent, msg, title, MessageBoxButton.YesNo, MessageBoxImage.Question);
+        }
+        public static void ShowInfo(Window parent, string msg, string title = "Info")
+        {
+            MessageBox.Show(parent, msg, title, MessageBoxButton.OK, MessageBoxImage.Information);
+        }
+    }
+}
