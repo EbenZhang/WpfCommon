d3886fe71d4b5c00972f75f9a3818aa84376a0dd
EbenZhang <zhangyingneng@gmail.com>
2015-03-02 09:11:00 +0800

    NullEmptyToFalseConverter.cs

---
+++ Converter/NullEmptyToFalseConverter.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Windows.Data;
+
+namespace WpfCommon.Converter
+{
+    [ValueConversion(typeof(object), typeof(bool))]
+    public class NullEmptyToFalseConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is string)
+            {
+                return string.IsNullOrWhiteSpace((string)value);
+            }
+            return value != null;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
