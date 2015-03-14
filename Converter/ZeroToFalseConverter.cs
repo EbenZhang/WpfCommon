031263efbd880c1196b27a13f6469514a7ec66bb
EbenZhang <zhangyingneng@gmail.com>
2015-03-14 22:19:04 +0800

    ZeroToFalseConverter

---
+++ Converter/ZeroToFalseConverter.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Windows.Data;
+
+namespace WpfCommon.Converter
+{
+    [ValueConversion(typeof(decimal), typeof(bool))]
+    public class ZeroToFalseConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value == null)
+            {
+                return false;
+            }
+            decimal number = 0;
+            if (decimal.TryParse(value.ToString(), out number))
+            {
+                return number != 0;
+            }
+            return false;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
