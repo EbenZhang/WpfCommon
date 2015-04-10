20a739776468a89846f11d6bc12e09513c1d43bd
EbenZhang <zhangyingneng@gmail.com>
2015-04-10 22:04:49 +0800

    add IsVisableConverter

---
+++ Converter/IsVisableConverter.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+
+namespace WpfCommon.Converter
+{
+    [ValueConversion(typeof(Visibility), typeof(bool))]
+    public class IsVisableConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return (Visibility) value == Visibility.Visible;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
