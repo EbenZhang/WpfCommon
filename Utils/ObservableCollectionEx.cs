4f04396953119122c4c286fb20bc37c99bb70170
EbenZhang <zhangyingneng@gmail.com>
2015-10-03 21:21:12 +0800

    Add ObservableCollectionEx

---
+++ Utils/ObservableCollectionEx.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using System.Collections.ObjectModel;
+
+namespace WpfCommon.Utils
+{
+    public static class ObservableCollectionEx
+    {
+        public static void AddRange<T>(this ObservableCollection<T> This, IEnumerable<T> collection)
+        {
+            foreach (var current in collection)
+            {
+                This.Add(current);
+            }
+        }
+    }
+}
