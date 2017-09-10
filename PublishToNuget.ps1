196ae12466f9040a3f0ff2905550dbc700bc3d01
EbenZhang <zhangyingneng@gmail.com>
2017-09-10 13:27:35 +0800

    should just push last nuget package

--- PublishToNuget.ps1
+++ PublishToNuget.ps1
@@ -4,5 +4,5 @@ param (
     [string]$apikey
 )
 $ErrorActionPreference = 'Stop'
-$file = gci -recurse -filter "Nicologies.WpfCommon.*.nupkg" -File -Path dist
+$file = gci -recurse -filter "Nicologies.WpfCommon.*.nupkg" -File -Path dist | sort -property LastWriteTime | Select-Object -Last 1
 nuget push $file.FullName -Source "https://www.nuget.org/api/v2/package" -ApiKey $apikey
